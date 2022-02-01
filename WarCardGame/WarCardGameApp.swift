0b9333563a7f6d674eb039a47a533d9811dd0e17
Evelina Semezyte <evelina.semezyte@gmail.com>
2022-02-01 20:18:26 +0200

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Evelina Semezyte on 2022-01-24.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
